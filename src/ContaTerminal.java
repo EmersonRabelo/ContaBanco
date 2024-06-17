fcd9dd327692f93729aae33c62fe5e2ae441fb39
EmersonRabelo <emersonrabelo1998@gmail.com>
2024-06-16 23:49:25 -0300

    Add: Data input with scanner.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
+        int numero;
+        String agencia;
+        String nome;
+        double saldo;
+
+        System.out.print("\nPor favor insira os seguintes dados.\n");
+
+        System.out.print("Número da conta: ");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Número da agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.print("Seu nome: ");
+        nome = scanner.nextLine();
+
+        System.out.print("Seu saldo: ");
+        saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta com nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
